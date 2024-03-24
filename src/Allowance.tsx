533d537cc7b7c70bfab7821c6987c0c89693d499
mbenz12 <mbenz10120112@gmail.com>
2024-03-24 13:07:26 -0700

    nft approvement feature added

--- src/Allowance.tsx
+++ src/Allowance.tsx
@@ -2,11 +2,13 @@
 import { useCallback, useEffect, useState } from "react";
 import { WalletConnectButton, useCosmWasmClient, useSigningCosmWasmClient, useWallet } from "@sei-js/react";
 
-const CONTRACT_ADDRESS = "sei13ff4qe9tja734jvflp6tk6laz3wpjdde0gyua8w0qj727xlm7zzsw6gptr";
+const FT_CONTRACT_ADDRESS = "sei13ff4qe9tja734jvflp6tk6laz3wpjdde0gyua8w0qj727xlm7zzsw6gptr";
+const NFT_CONTRACT_ADDRESS = "sei1geqkhqff9lpvg0yj58yg9wkawy86gn3qtt4mytdau46nqqx0cdus47yl5d";
 
 function Allowance() {
     const [error, setError] = useState<string>("");
-    const [contractAddress, setContractAddress] = useState(CONTRACT_ADDRESS);
+    const [isNFT, setIsNFT] = useState(false);
+    const [contractAddress, setContractAddress] = useState(FT_CONTRACT_ADDRESS);
     const { connectedWallet, accounts } = useWallet();
     const { cosmWasmClient: queryClient } = useCosmWasmClient();
     const { signingCosmWasmClient: signingClient } = useSigningCosmWasmClient();
@@ -15,29 +17,58 @@ function Allowance() {
     const [spenderAddress, setSpenderAddress] = useState('');
     const [amount, setAmount] = useState(0);
     const [balance, setBalance] = useState('');
-    const [tokenInfo, setTokenInfo] = useState({});
+    const [tokenInfo, setTokenInfo] = useState<any>({});
+    const [approvals, setApprovals] = useState<Array<Array<any>>>([]);
+    const [tokens, setTokens] = useState<Array<any>>([]);
 
     const fetchToken = useCallback(async () => {
         if (!accounts.length || !contractAddress) return;
         const senderAddress = accounts[0].address;
 
-        const balance = (await queryClient?.queryContractSmart(contractAddress, {
-            balance: {
-                "address": senderAddress
-            },
-        }))?.balance;
-        const tokenInfo = (await queryClient?.queryContractSmart(contractAddress, {
-            token_info: {},
-        }));
-        const allowances = (await queryClient?.queryContractSmart(contractAddress, {
-            all_allowances: {
-                "owner": senderAddress
-            },
-        }))?.allowances;
-        return { balance, tokenInfo, allowances };
+        if (isNFT) {
+            const response = await queryClient?.queryContractSmart(contractAddress, {
+                tokens: {
+                    "owner": senderAddress
+                }
+            });
+            const tokens = response?.tokens || [];
+            const approvals = await Promise.all(tokens.map(async (token: string) => await queryClient?.queryContractSmart(contractAddress, {
+                approvals: {
+                    "token_id": token
+                }
+            })));
+
+            const tokenInfo = await queryClient?.queryContractSmart(contractAddress, {
+                contract_info: {}
+            });
+
+            setTokens(tokens || []);
+            setApprovals(approvals.map(token => token.approvals) || []);
+            setTokenInfo(tokenInfo);
+        } else {
+            const [balance, tokenInfo, allowances] = await Promise.all([
+                queryClient?.queryContractSmart(contractAddress, {
+                    balance: {
+                        "address": senderAddress
+                    },
+                }),
+                queryClient?.queryContractSmart(contractAddress, {
+                    token_info: {},
+                }),
+                queryClient?.queryContractSmart(contractAddress, {
+                    all_allowances: {
+                        "owner": senderAddress
+                    },
+                })
+            ]);
+
+            setBalance(balance?.balance);
+            setAllowances(allowances?.allowances);
+            setTokenInfo(tokenInfo);
+        }
     }, [queryClient, accounts, contractAddress]);
 
-    const revoke = async (allowance: any) => {
+    const revokeFT = async (allowance: any) => {
         if (!accounts.length) return;
         setIsRunning(true);
 
@@ -59,8 +90,7 @@ function Allowance() {
             // Call smart contract execute msg
             await signingClient?.execute(senderAddress, contractAddress, msg, fee);
 
-            const updatedAllowances = await fetchAllowance();
-            setAllowances(updatedAllowances);
+            await fetchToken();
             setIsRunning(false);
             setError("");
         } catch (error) {
@@ -80,7 +110,7 @@ function Allowance() {
         try {
             const senderAddress = accounts[0].address;
 
-            const msg = amount > 0 ?  {
+            const msg = amount > 0 ? {
                 increase_allowance: {
                     spender: spenderAddress,
                     amount: amount.toString(),
@@ -100,8 +130,77 @@ function Allowance() {
             // Call smart contract execute msg
             await signingClient?.execute(senderAddress, contractAddress, msg, fee);
 
-            const updatedAllowances = await fetchAllowance();
-            setAllowances(updatedAllowances);
+            await fetchToken();
+            setIsRunning(false);
+            setError("");
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            } else {
+                setError("unknown error");
+            }
+            setIsRunning(false);
+        }
+    }
+
+    const revokeNFT = async (token_id: string, spender: string) => {
+        if (!accounts.length) return;
+        setIsRunning(true);
+
+        try {
+            const senderAddress = accounts[0].address;
+
+            const msg = {
+                revoke: {
+                    spender,
+                    token_id
+                }
+            };
+
+            const fee = {
+                amount: [{ amount: "20000", denom: "usei" }],
+                gas: "200000",
+            };
+
+            // Call smart contract execute msg
+            await signingClient?.execute(senderAddress, contractAddress, msg, fee);
+
+            await fetchToken();
+            setIsRunning(false);
+            setError("");
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            } else {
+                setError("unknown error");
+            }
+            setIsRunning(false);
+        }
+    }
+
+    const approveNFT = async (token_id: string) => {
+        if (!accounts.length) return;
+        setIsRunning(true);
+
+        try {
+            const senderAddress = accounts[0].address;
+
+            const msg = {
+                approve: {
+                    spender: spenderAddress,
+                    token_id,
+                }
+            };
+
+            const fee = {
+                amount: [{ amount: "20000", denom: "usei" }],
+                gas: "200000",
+            };
+
+            // Call smart contract execute msg
+            await signingClient?.execute(senderAddress, contractAddress, msg, fee);
+
+            await fetchToken();
             setIsRunning(false);
             setError("");
         } catch (error) {
@@ -115,36 +214,83 @@ function Allowance() {
     }
 
     useEffect(() => {
-        fetchToken().then((token) => {
-            setBalance(token?.balance);
-            setAllowances(token?.allowances);
-            setTokenInfo(token?.tokenInfo);
-            console.log(token);
-        })
+        fetchToken();
     }, [connectedWallet, fetchToken]);
+
+    useEffect(() => {
+        setContractAddress(isNFT ? NFT_CONTRACT_ADDRESS : FT_CONTRACT_ADDRESS);
+    }, [isNFT]);
 
     if (!connectedWallet) return <WalletConnectButton />;
 
     return (
         <div>
             <div>
-                Contract Address: 
-                <input value={contractAddress} onChange={(e) => { setContractAddress(e.target.value) }} />                
-                <div>Balance: {balance}</div>
-                {tokenInfo && <div>Token Info- decimals: {tokenInfo?.decimals} name: {tokenInfo?.name} symbol: {tokenInfo?.symbol}</div>}
+                <h2>Select Token Type:</h2>
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            name="tokenType"
+                            value="NFT"
+                            checked={isNFT}
+                            onChange={() => setIsNFT(true)}
+                        />
+                        NFT (Non-Fungible Token)
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            name="tokenType"
+                            value="FT"
+                            checked={!isNFT}
+                            onChange={() => setIsNFT(false)}
+                        />
+                        FT (Fungible Token)
+                    </label>
+                </div>
+            </div>
+            <div>
+                Contract Address:
+                <input value={contractAddress} onChange={(e) => { setContractAddress(e.target.value) }} />
+                {!isNFT && <div>Balance: {balance}</div>}
+                {tokenInfo && <div>{tokenInfo?.decimals && <>Decimals: {tokenInfo?.decimals}</>} Name: {tokenInfo?.name} Symbol: {tokenInfo?.symbol}</div>}
             </div>
             <div>
                 Spender: <input value={spenderAddress} onChange={(e) => setSpenderAddress(e.target.value)} />
-                Amount: <input value={amount} onChange={(e) => setAmount(parseInt(e.target.value))} />
-                <button disabled={isRunning} onClick={() => increaseAllowance()} >Allow</button>
+                {!isNFT && <>
+                    Amount: <input value={amount} onChange={(e) => setAmount(parseInt(e.target.value))} />
+                </>}
             </div>
-            {allowances && allowances.map((allowance) => (
-                <div key={allowance.spender}>
-                    <div>{allowance.spender}</div>
-                    <div>{allowance.allowance}</div>
-                    <button disabled={isRunning} onClick={() => revoke(allowance)}>Revoke</button>
+            {!isNFT ? <>
+                <div>
+                    <button disabled={isRunning} onClick={() => increaseAllowance()} >Allow</button>
                 </div>
-            ))}
+                {allowances && allowances.map((allowance) => (
+                    <div key={allowance.spender}>
+                        <div>{allowance.spender}</div>
+                        <div>{allowance.allowance}</div>
+                        <button disabled={isRunning} onClick={() => revokeFT(allowance)}>Revoke</button>
+                    </div>
+                ))}
+            </> : <>
+                {tokens.map((token, tokenIndex) => (
+                    <div key={token}>
+                        <h4>{token}</h4>
+                        <div>
+                            <button disabled={isRunning} onClick={() => approveNFT(token)}>Approve</button>
+                            {approvals[tokenIndex].map(approval => (
+                                <div key={approval.spender}>
+                                    <div>{approval.spender}</div>
+                                    <button disabled={isRunning} onClick={() => revokeNFT(token, approval.spender)}>Revoke</button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </>}
             {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
     );
